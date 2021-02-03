6f4c7d7dfb3d6c8d19e28aeef7c4358d637152e0
Samet195 <70340667+Samet195@users.noreply.github.com>
2021-02-03 14:32:52 +0300

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -5,28 +5,28 @@ const title = doc.querySelector("title").innerText;
 let li = doc.querySelectorAll("nav>li");
 
 function render(page) {
-	doc.querySelector("title").innerText = title + view[page].title;
-	/**************************************************************/
-	li.forEach(
-		item => {
-			if (item.getAttribute("id") == page) {
-				item.className = "active"
-			} else {
-				item.removeAttribute("class")
-			}
-		}
-	);
-	/**************************************************************/
-	doc.querySelector("main").innerHTML = view[page].content;
+    doc.querySelector("title").innerText = title + view[page].title;
+    /**************************************************************/
+    li.forEach(
+        item => {
+            if (item.getAttribute("id") == page) {
+                item.className = "active"
+            } else {
+                item.removeAttribute("class")
+            }
+        }
+    );
+    /**************************************************************/
+    doc.querySelector("main").innerHTML = view[page].content;
 };
 
 li.forEach(
-	item => item.addEventListener(
-		"click",
-		() => {
-			render(item.getAttribute("id"))
-		}
-	)
+    item => item.addEventListener(
+        "click",
+        () => {
+            render(item.getAttribute("id"))
+        }
+    )
 );
 
 render("anasayfa");
