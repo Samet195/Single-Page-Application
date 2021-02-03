9b8d8bad5cedda86b47cfe0cd7fbeec1a685c11b
Samet195 <70340667+Samet195@users.noreply.github.com>
2021-02-03 14:36:57 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from webbrowser import open
 
 if __name__ == '__main__':
     try:
         print(f"Server started at http://localhost:8000/\n{'=' * 40}")
+        open("http://localhost:8000/")
         HTTPServer(("localhost", 8000), SimpleHTTPRequestHandler).serve_forever()
     except KeyboardInterrupt:
         print("Server stopped")
