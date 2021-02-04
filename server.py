01ba6cf39fab366676110762c5348b7e9cc97cd8
Samet195 <70340667+Samet195@users.noreply.github.com>
2021-02-04 21:10:34 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-from webbrowser import open
+import webbrowser
 
 if __name__ == '__main__':
     try:
         print(f"Server started at http://localhost:8000/\n{'=' * 40}")
-        open("http://localhost:8000/")
+        webbrowser.open("http://localhost:8000/")
         HTTPServer(("localhost", 8000), SimpleHTTPRequestHandler).serve_forever()
     except KeyboardInterrupt:
         print("Server stopped")
